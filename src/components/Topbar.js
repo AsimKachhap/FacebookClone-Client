34bcd3432ea432507ddb4bc53d8f14b7e264732a
unknown <asim26k@gmail.com>
2023-01-27 15:46:07 +0530

    installed react-icons as material-icons didn't work

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,11 +1,11 @@
 import React from "react";
-import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
 
 const Topbar = () => {
   return (
     <div className="flex flex-row justify-between px-2 py-1 ">
-      <div>
-        <FacebookOutlinedIcon />
+      <div className="flex flex-row gap-2">
+        <p>Facebook logo</p>
+        <p>Search bar</p>
       </div>
       <div>2</div>
       <div>3</div>
