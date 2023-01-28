cba6c77cbe4709aa44b15b638dd99cb4e2944e85
unknown <asim26k@gmail.com>
2023-01-28 07:22:08 +0530

    fixed search bar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -11,7 +11,11 @@ const Topbar = () => {
         <BsFacebook className="text-blue-500 h-10 w-10" />
         <div className="h-10 w-56 bg-[#E6E6E6] rounded-full flex flex-row items-center text-[#687A86] gap-2 px-2 ">
           <GoSearch className="font-bold " />
-          <p>Search Facebook</p>
+          <input
+            className=" bg-transparent focus:outline-none "
+            type="text"
+            placeholder="Search Facebook"
+          />
         </div>
       </div>
 
